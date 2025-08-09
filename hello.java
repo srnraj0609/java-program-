87250981e248bc7e9601248bee6630412b5ddfcc
srnraj0609 <srnraj0609@gmail.com>
2025-08-09 07:58:30 +0530

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,10 +1,13 @@
-public class hello {
-    public static void main(String args[] )
-     {
-        int a=10;
-        int b=20;
-        int c=30;
-        System.out.print(a+b+c);
+import java.util.Scanner;
+class hello{
+    public static void main(String[] args)
+    {
+        Scanner saran = new Scanner(System.in);
+        int a=saran.nextInt();
+        int b=saran.nextInt();
+        System.out.print(a);
+        System.out.print(b);
+        saran.close();
     }
-}
 
+}
